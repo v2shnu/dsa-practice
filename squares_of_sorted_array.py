e6f2d6c0d7fffeb638761d3b5ac0fa84443bd7b5
v2shnu <ps4vishnu@gmail.com>
2026-01-13 22:39:08 +0530

    Update squares_of_sorted_array.py

--- squares_of_sorted_array.py
+++ squares_of_sorted_array.py
@@ -3,3 +3,7 @@ def sortedSquares(nums):
             nums[i]=nums[i]**2
         sortednums = sorted(nums)
         return sortednums
+        
+# NOTE:
+# This solution works but runs in O(n log n) due to sorting.
+# An optimized O(n) solution exists using two pointers.
