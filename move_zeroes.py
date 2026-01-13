f498f2dcbcf662efff83362b6aced023acb64143
v2shnu <ps4vishnu@gmail.com>
2026-01-13 22:14:59 +0530

    Create move_zeroes.py

---
+++ move_zeroes.py
@@ -0,0 +1,7 @@
+def moveZeroes(nums):
+        i=0
+        for j in range(len(nums)):
+            if nums[j]!=0:
+                nums[i],nums[j]=nums[j],nums[i]
+                i+=1
+        return nums
